5bfb856996da11c8940673b71387be83d814c1bc
lauchaventura <lautaroventura02@gmail.com>
2022-03-15 11:29:17 -0300

    fixed iframe bug

--- components/Map.js
+++ components/Map.js
@@ -18,10 +18,10 @@ const Map = () => {
                 <Box boxShadow="2xl"
                     backgroundColor="#fff"  >
                     <Box overflow='hidden' paddingBottom='400' position='relative' height='0' >
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3099.8523276380256!2d-67.63667628489308!3d-39.01868201171285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a1dfbb50cc07b%3A0xce26a880883c9fcf!2sRadio%20Agua%20de%20Vida!5e0!3m2!1ses!2sar!4v1647307509711!5m2!1ses!2sar" width="600" height="450" allowfullScreen="" loading="lazy"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3099.8523276380256!2d-67.63667628489308!3d-39.01868201171285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a1dfbb50cc07b%3A0xce26a880883c9fcf!2sRadio%20Agua%20de%20Vida!5e0!3m2!1ses!2sar!4v1647307509711!5m2!1ses!2sar" width="600" height="450" loading="lazy"></iframe>
                     </Box>
                 </Box>
-                <Box>
+                <Box >
                     <Text>
                         Nuestros estudios ubicados en General Roca, Rio Negro
                     </Text>
